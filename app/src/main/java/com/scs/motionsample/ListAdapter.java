07e375dccdddb040387c7af1640aadde5a537472
Darrel Rayen <darrel.r@aeturnum.com>
2022-11-12 23:03:02 +0530

    Initial commit

---
+++ app/src/main/java/com/scs/motionsample/ListAdapter.java
@@ -0,0 +1,47 @@
+package com.scs.motionsample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
+    private List<String> nameList;
+
+    public ListAdapter(List<String> nameList) {
+        this.nameList = nameList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(nameList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return nameList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text_name);
+        }
+    }
+}
